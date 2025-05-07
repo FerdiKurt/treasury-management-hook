c6487040f4321a9f8191f4534d8b257ff0a44ec6
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-05-07 15:58:12 +0300

    Testing imports

---
+++ src/TestImports.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v4-core/src/libraries/Hooks.sol";
+import "@uniswap/v4-periphery/src/utils/BaseHook.sol";
+
+// This contract is just to test imports - it doesn't need any content
+contract TestImports {
+    // Empty contract
+}
